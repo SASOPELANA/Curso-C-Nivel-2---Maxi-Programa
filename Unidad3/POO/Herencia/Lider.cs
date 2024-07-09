1f03e131434db36732a9d72f8501b036103ad97e
sergio <sopekof@gmail.com>
2024-07-09 15:14:05 -0300

    Herencia en C#

---
+++ Unidad3/POO/Herencia/Lider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herencia
+{
+    internal class Lider : Persona // Herencia en C#
+    {
+
+
+    }
+}
---
+++ Unidad3/POO/Herencia/Persona.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herencia
+{
+    internal class Persona
+    {
+        // Clase padre o principal.
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int legajo { get; set; }
+
+    }
+}
