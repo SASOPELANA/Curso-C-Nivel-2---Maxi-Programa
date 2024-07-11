daf3069cf4beaa2ea84bc6c5933e72fd7329c9c1
sergio <sopekof@gmail.com>
2024-07-11 03:25:42 -0300

    Aplicacion WinForms

---
+++ Unidad4/PrimeraSolucionWinForms/Ejemplo1/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Ejemplo1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSaludar = new System.Windows.Forms.Button();
+            this.lblSaludo = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSaludar
+            // 
+            this.btnSaludar.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnSaludar.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnSaludar.Location = new System.Drawing.Point(274, 159);
+            this.btnSaludar.Name = "btnSaludar";
+            this.btnSaludar.Size = new System.Drawing.Size(198, 106);
+            this.btnSaludar.TabIndex = 0;
+            this.btnSaludar.Text = "Saludar";
+            this.btnSaludar.UseVisualStyleBackColor = false;
+            this.btnSaludar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblSaludo
+            // 
+            this.lblSaludo.AutoSize = true;
+            this.lblSaludo.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblSaludo.Location = new System.Drawing.Point(339, 287);
+            this.lblSaludo.Name = "lblSaludo";
+            this.lblSaludo.Size = new System.Drawing.Size(0, 13);
+            this.lblSaludo.TabIndex = 1;
+            this.lblSaludo.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(345, 121);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 2;
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.BackColor = System.Drawing.Color.DarkGray;
+            this.lblNombre.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblNombre.Location = new System.Drawing.Point(289, 124);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(50, 13);
+            this.lblNombre.TabIndex = 3;
+            this.lblNombre.Text = "Nombre: ";
+           
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkGray;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.lblSaludo);
+            this.Controls.Add(this.btnSaludar);
+            this.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.Name = "Form1";
+            this.Text = "Primera APP";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSaludar;
+        private System.Windows.Forms.Label lblSaludo;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label lblNombre;
+    }
+}
+
---
+++ Unidad4/PrimeraSolucionWinForms/Ejemplo1/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejemplo1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Carga una ventana previa, antes de ejecutar la App.
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           // MessageBox.Show("Te Saludo");
+        }
+
+        // Método especial para ejecutar el button. Evento por estandar
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // METODO PARA INVOCAR UNA UN BOTON. ACCION DE EVENTOS CLICK BUTTON
+            // MessageBox.Show("Hola Mundo"); // CREA UNA VENTANA 
+
+            // Capturamos el nombre en la variable tipo string  texto
+            string texto = txtNombre.Text;
+            lblSaludo.Text = "Hola " + texto;
+
+        }
+
+        // Dando click a un evento se crea una funcion en el archvio Form1.cs
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+           // MessageBox.Show("Hasta Luego...");
+        }
+
+       
+
+    }
+}
